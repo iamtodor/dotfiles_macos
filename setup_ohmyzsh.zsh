8dc01eb1fa1b51150110cd46fe49271d7f07f2a0
illia.todor <todor.ilya@gmail.com>
2024-03-24 12:55:20 +0100

    fix install ohmyzsh

--- setup_ohmyzsh.zsh
+++ setup_ohmyzsh.zsh
@@ -4,6 +4,8 @@ echo "\n<<< Starting oh-my-zsh Setup >>>\n"
 
 if [ -z "$ZSH" ]; then
     echo "\$ZSH is not set, installing Oh My Zsh"
+    mv ~/.oh-my-zsh/ ~/.oh-my-zsh_past/
+    mv ~/.zshrc ~/.zshrc_past
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
     echo "\$ZSH is already set as $ZSH, skipping Oh My Zsh installation"
